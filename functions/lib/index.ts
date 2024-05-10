d45df844a40b28fb1eac19bb5e7f5d9c9ba6fdda
choisho <choisho1136@gmail.com>
2024-05-10 12:31:25 +0900

    Fix: 서버 반환 변수명 수정

--- functions/lib/index.ts
+++ functions/lib/index.ts
@@ -1,19 +1,14 @@
 import {HttpsOptions, onRequest} from "firebase-functions/v2/https";
-import { initializeApp } from "firebase-admin/app";
 import  * as express  from 'express';
 
 import districtRoutes from "./routes/districtRoutes";
 
 
-initializeApp();
-
-const httpsOptoins: HttpsOptions = {region: 'asia-northeast3'};
+const httpsOptoins: HttpsOptions = { region: 'asia-northeast3'};
 
 const app = express();
-
 
 app.use('/district', districtRoutes);
 
 
-export const api = onRequest(httpsOptoins, app);
-
+exports.app = onRequest(httpsOptoins, app);

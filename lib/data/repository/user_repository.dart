6cdedac617446270f71cedb33e93836b96cd6605
choisho <choisho1136@gmail.com>
2024-05-27 18:21:51 +0900

    Add: UserREpository create, login 작성

---
+++ lib/data/repository/user_repository.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logger/logger.dart';
+import 'package:logger/web.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:avocado/util/api.dart';
+import 'package:avocado/util/shared_preferences.dart';
+
+part 'user_repository.g.dart';
+
+@riverpod
+class UserRepository extends _$UserRepository {
+  final String _url = 'bioni-avocado.firebaseapp.com';
+
+  @override
+  void build() {}
+
+  Future<String?> create() async {
+    try {
+      var response = await ApiUtil.get(_url, '/auth/create');
+
+      String token = response['token'];
+
+      await FirebaseAuth.instance.signInWithCustomToken(token);
+
+      await SharedPreferencesUtil().setString('token', token);
+
+      return token;
+    } catch (e) {
+      Logger().e(e);
+      return null;
+    }
+  }
+
+  Future<void> login(String token) async {
+    try {
+      String? idToken = await FirebaseAuth.instance.currentUser?.getIdToken();
+
+      if (idToken != null) {
+        var user = await ApiUtil.post(_url, '/auth/login', {'token': idToken});
+        Logger().d(user);
+      } else {
+        throw 'idToken 이 존재하지 않습니다.';
+      }
+    } catch (error) {
+      Logger().e(error);
+      // TODO: 로그인 error 처리
+    }
+  }
+}
---
+++ lib/data/repository/user_repository.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userRepositoryHash() => r'e57bc8af611d78bf6a0647cd8a7d6a166d751b12';
+
+/// See also [UserRepository].
+@ProviderFor(UserRepository)
+final userRepositoryProvider =
+    AutoDisposeNotifierProvider<UserRepository, void>.internal(
+  UserRepository.new,
+  name: r'userRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserRepository = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

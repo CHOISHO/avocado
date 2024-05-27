f63af3cd432a4158e2ed74883783e46882a0a1e3
choisho <choisho1136@gmail.com>
2024-05-27 16:52:13 +0900

    Add: ApiUtil Post 메소드

--- lib/util/api.dart
+++ lib/util/api.dart
@@ -10,9 +10,22 @@ Map<String, String> baseHeaders = {
 class ApiUtil {
   static Future<Map<String, dynamic>> get(String url, String path,
       [Map<String, String>? queryParameters]) async {
-    Uri uri = Uri.http(url, path, queryParameters);
+    Uri uri = Uri.https(url, path, queryParameters);
 
     http.Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('${response.statusCode} ${response.body}');
+    }
+  }
+
+  static Future<Map<String, dynamic>> post(String url, String path,
+      [Map<String, String>? body]) async {
+    Uri uri = Uri.https(url, path);
+
+    http.Response response = await http.post(uri, body: body);
 
     if (response.statusCode == 200) {
       return json.decode(response.body);

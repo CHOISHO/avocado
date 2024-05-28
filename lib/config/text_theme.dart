a4ab4d684113eae9e71319e52f807f819ebe913d
choisho <choisho1136@gmail.com>
2024-05-28 13:35:11 +0900

    Add: Text Theme 기본 색상, 굵기 추가

--- lib/config/text_theme.dart
+++ lib/config/text_theme.dart
@@ -1,37 +1,54 @@
 import 'package:flutter/material.dart';
 
-TextTheme textTheme = const TextTheme(
+import 'package:avocado/config/avocado_colors.dart';
+
+TextTheme textTheme = TextTheme(
   titleMedium: TextStyle(
     fontFamily: 'Pretendard',
     fontSize: 20,
     letterSpacing: -0.25,
+    fontWeight: FontWeight.w400,
+    color: AvocadoColors.black,
   ),
   titleSmall: TextStyle(
     fontFamily: 'Pretendard',
     fontSize: 18,
     letterSpacing: -0.25,
+    fontWeight: FontWeight.w400,
+    color: AvocadoColors.black,
   ),
   bodyMedium: TextStyle(
     fontFamily: 'Pretendard',
     fontSize: 16,
     letterSpacing: -0.25,
+    fontWeight: FontWeight.w400,
+    color: AvocadoColors.black,
   ),
   bodySmall: TextStyle(
     fontFamily: 'Pretendard',
     fontSize: 14,
     letterSpacing: -0.25,
+    fontWeight: FontWeight.w400,
+    color: AvocadoColors.black,
   ),
   labelMedium: TextStyle(
     fontFamily: 'Pretendard',
     fontSize: 12,
     letterSpacing: -0.25,
+    fontWeight: FontWeight.w400,
+    color: AvocadoColors.black,
   ),
 );
 
 extension UIThemeExtension on BuildContext {
-  TextStyle get textThemeTitle1 => Theme.of(this).textTheme.titleMedium ?? const TextStyle();
-  TextStyle get textThemeTitle2 => Theme.of(this).textTheme.titleSmall ?? const TextStyle();
-  TextStyle get textThemeBody1 => Theme.of(this).textTheme.bodyMedium ?? const TextStyle();
-  TextStyle get textThemeBody2 => Theme.of(this).textTheme.bodySmall ?? const TextStyle();
-  TextStyle get textThemeLabel => Theme.of(this).textTheme.labelMedium ?? const TextStyle();
+  TextStyle get textThemeTitle1 =>
+      Theme.of(this).textTheme.titleMedium ?? const TextStyle();
+  TextStyle get textThemeTitle2 =>
+      Theme.of(this).textTheme.titleSmall ?? const TextStyle();
+  TextStyle get textThemeBody1 =>
+      Theme.of(this).textTheme.bodyMedium ?? const TextStyle();
+  TextStyle get textThemeBody2 =>
+      Theme.of(this).textTheme.bodySmall ?? const TextStyle();
+  TextStyle get textThemeLabel =>
+      Theme.of(this).textTheme.labelMedium ?? const TextStyle();
 }

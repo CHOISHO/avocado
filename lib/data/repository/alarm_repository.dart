1bcafba22cd807be10ee647458a7de5f16a3b125
choisho <choisho1136@gmail.com>
2024-05-30 20:24:41 +0900

    WIP: 알람 추가

---
+++ lib/data/repository/alarm_repository.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:logger/logger.dart';
+
+import 'package:avocado/domain/model/alarm_model.dart';
+import 'package:avocado/util/shared_preferences.dart';
+
+class AlarmRepository {
+  factory AlarmRepository() {
+    return _instance;
+  }
+
+  AlarmRepository._internal();
+
+  static final AlarmRepository _instance = AlarmRepository._internal();
+
+  Future<void> addAlarm(AlarmModel alarm) async {
+    try {
+      List<String> alarms =
+          SharedPreferencesUtil().getStringList('alarms')?.toList() ?? [];
+
+      await SharedPreferencesUtil()
+          .setStringList('alarms', [...alarms, jsonEncode(alarm.toJson())]);
+    } catch (error) {
+      Logger().e(error);
+    }
+  }
+
+  List<AlarmModel> getAlarms() {
+    List<String> alarms =
+        SharedPreferencesUtil().getStringList('alarms')?.toList() ?? [];
+
+    var parsedAlarms =
+        alarms.map((e) => AlarmModel.fromJson(jsonDecode(e))).toList();
+
+    return parsedAlarms;
+  }
+}
--- lib/feature/view_model/home_view_model.dart
+++ lib/feature/view_model/home_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import 'package:avocado/data/repository/alarm_repository.dart';
 import 'package:avocado/data/repository/weather_repository.dart';
 import 'package:avocado/domain/model/alarm_model.dart';
 import 'package:avocado/domain/model/weather_model.dart';
@@ -33,9 +34,11 @@ class HomeViewModel extends _$HomeViewModel {
         .watch(weatherRepositoryProvider.notifier)
         .getUltraShortTermForecast();
 
+    List<AlarmModel> alarms = AlarmRepository().getAlarms();
+
     final previousState = state.asData!.value;
     state = AsyncData(
-      previousState.copyWith(weather: weather),
+      previousState.copyWith(weather: weather, alarms: alarms),
     );
   }
 
@@ -49,6 +52,8 @@ class HomeViewModel extends _$HomeViewModel {
   }
 
   void addAlarm(AlarmModel alarm) {
+    AlarmRepository().addAlarm(alarm);
+
     final previousState = state.asData!.value;
 
     state = AsyncData(

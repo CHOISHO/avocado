d7dd8fe989a9599fe2acdc92b077b9a9d94fd50f
choisho <choisho1136@gmail.com>
2024-06-06 22:04:54 +0900

    Add: ApiUtil Authorization token

--- lib/util/api.dart
+++ lib/util/api.dart
@@ -4,15 +4,19 @@ import 'package:http/http.dart' as http;
 
 Map<String, String> baseHeaders = {
   'Content-Type': 'application/json',
-  'Accept': 'application/json',
 };
 
 class ApiUtil {
   static Future<Map<String, dynamic>> get(String url, String path,
-      [Map<String, String>? queryParameters]) async {
-    Uri uri = Uri.https(url, path, queryParameters);
+      [Map<String, String>? queryParameters, String? token]) async {
+    Uri uri = Uri.https(Uri.encodeComponent(url), path, queryParameters);
 
-    http.Response response = await http.get(uri);
+    http.Response response = await http.get(
+      uri,
+      headers: token != null
+          ? baseHeaders
+          : {...baseHeaders, 'Authorization': '$token'},
+    );
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
@@ -21,11 +25,24 @@ class ApiUtil {
     }
   }
 
-  static Future<Map<String, dynamic>> post(String url, String path,
-      [Map<String, String>? body]) async {
+  static Future<Map<String, dynamic>> post({
+    required String url,
+    required String path,
+    required Map<String, dynamic> body,
+    String? token,
+  }) async {
     Uri uri = Uri.https(url, path);
 
-    http.Response response = await http.post(uri, body: body);
+    http.Response response = await http.post(
+      uri,
+      headers: token != null
+          ? baseHeaders
+          : {
+              ...baseHeaders,
+              'Authorization': '$token',
+            },
+      body: jsonEncode(body),
+    );
 
     if (response.statusCode == 200) {
       return json.decode(response.body);

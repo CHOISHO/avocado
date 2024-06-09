ab04c7b1b43157c5ea00730be9adc3227faff131
choisho <choisho1136@gmail.com>
2024-06-09 15:41:27 +0900

    Add: typescript-json-serializer

--- functions/lib/index.ts
+++ functions/lib/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import { HttpsOptions, onRequest } from 'firebase-functions/v2/https';
+import { JsonSerializer } from 'typescript-json-serializer';
 
 import alarmRoutes from './routes/alarmRoutes';
 import authRoutes from './routes/authRoutes';
 import districtRoutes from './routes/districtRoutes';
+
+export const defaultSerializer = new JsonSerializer();
 
 const httpsOptoins: HttpsOptions = { region: 'asia-northeast3'};
 
